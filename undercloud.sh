f56e52676f2e7bcd808c8894c2030114897a6ae7
Victor Morales <victor.morales@intel.com>
2018-09-02 13:48:15 -0700

    Remove unnecessary instructions

--- undercloud.sh
+++ undercloud.sh
@@ -17,11 +17,6 @@ kolla_folder=/opt/kolla-ansible
 kolla_version=master
 kolla_tarball=kolla-ansible-$kolla_version.tar.gz
 
-# Setup proxy variables
-if [ -f /vagrant/sources.list ]; then
-    cat /vagrant/sources.list >> /etc/apt/sources.list
-fi
-
 apt install -y python2.7 python-dev build-essential sshpass
 curl -sL https://bootstrap.pypa.io/get-pip.py | sudo python
 
@@ -42,10 +37,6 @@ forks=100
 EOLF
 
 kolla-genpwd
-#ansible -i $inventory_file all -m raw -a "sudo apt-get -y install python-dev"
 for action in bootstrap-servers prechecks deploy post-deploy; do
     kolla-ansible -vvv -i $inventory_file $action | tee $action.log
-    if [[ "$action" == "bootstrap-servers" ]]; then
-        ansible -i $inventory_file all -m raw -a "sudo usermod -aG docker vagrant"
-    fi
 done
