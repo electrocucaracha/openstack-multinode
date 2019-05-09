dfb2b8bacc47aeea148f5a0917882c070db89a31
Victor Morales <victor.morales@intel.com>
2019-05-09 11:17:44 -0700

    Update Kolla-Ansible to Stein release
    This change includes the instructions for deploying OpenStack Stein
    release.

--- undercloud.sh
+++ undercloud.sh
@@ -13,9 +13,9 @@ set -o nounset
 set -o pipefail
 
 # Variables
-inventory_file=/vagrant/inventory/hosts.ini
+inventory_file=./inventory/hosts.ini
 kolla_folder=/opt/kolla-ansible
-kolla_version=stable-rocky
+kolla_version=stable-stein
 kolla_tarball=kolla-ansible-$kolla_version.tar.gz
 
 sudo apt install -y python2.7 python-dev build-essential sshpass
@@ -23,8 +23,10 @@ curl -sL https://bootstrap.pypa.io/get-pip.py | sudo python
 
 wget http://tarballs.openstack.org/kolla-ansible/$kolla_tarball
 sudo tar -C /opt -xzf $kolla_tarball
+rm $kolla_tarball
 sudo mv /opt/kolla-*/ $kolla_folder
 sudo cp $kolla_folder/etc/kolla/passwords.yml /etc/kolla/
+sudo -E -H pip install --upgrade ansible
 sudo -E -H pip install $kolla_folder
 
 sudo -E -H pip install python-openstackclient
@@ -36,6 +38,7 @@ echo "pipelinig=True" | sudo tee --append /etc/ansible/ansible.cfg
 echo "forks=100" | sudo tee --append /etc/ansible/ansible.cfg
 
 sudo kolla-genpwd
+sudo mkdir -p /var/log/kolla/
 for action in bootstrap-servers prechecks pull deploy check post-deploy; do
-    sudo kolla-ansible -vvv -i $inventory_file $action -e ansible_user=kolla | tee $action.log
+    sudo kolla-ansible -vvv -i $inventory_file $action -e 'ansible_user=kolla' -e 'ansible_become=true' -e 'ansible_become_method=sudo' | tee $action.log
 done
