6fd44a298c9fd7591ca9b351880f60314df3a915
Victor Morales <chipahuac@hotmail.com>
2021-07-01 11:31:36 -0700

    Improve Compute instructions

--- compute.sh
+++ compute.sh
@@ -13,10 +13,26 @@ set -o pipefail
 set -o errexit
 set -o xtrace
 
-# shellcheck disable=SC1091
-source /etc/os-release || source /usr/lib/os-release
-case ${ID,,} in
-    rhel|centos|fedora)
-        eval "sudo -H -E $(command -v dnf || command -v yum) -y install libvirt-client"
-    ;;
-esac
+if ! command -v curl; then
+    # shellcheck disable=SC1091
+    source /etc/os-release || source /usr/lib/os-release
+    case ${ID,,} in
+        ubuntu|debian)
+            sudo apt-get update
+            sudo apt-get install -y -qq -o=Dpkg::Use-Pty=0 --no-install-recommends curl
+        ;;
+    esac
+fi
+# Install dependencies
+pkgs=""
+for pkg in docker pip bindep; do
+    if ! command -v "$pkg"; then
+        pkgs+=" $pkg"
+    fi
+done
+if [ -n "$pkgs" ]; then
+    # NOTE: Shorten link -> https://github.com/electrocucaracha/pkg-mgr_scripts
+    curl -fsSL http://bit.ly/install_pkg | PKG=$pkgs bash
+fi
+curl -fsSL http://bit.ly/install_pkg | PKG="$(bindep compute -b)" bash
+sudo pip install docker
