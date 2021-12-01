366a1f660be2ac99b83ed7f7d1dacd519e9136f6
Victor Morales <chipahuac@hotmail.com>
2021-11-30 18:31:05 -0800

    Add Neutron Trunk demo

---
+++ scripts/magnum.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2020
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+set -o nounset
+set -o pipefail
+set -o errexit
+if [[ "${OS_DEBUG:-false}" == "true" ]]; then
+    export PKG_DEBUG=true
+    set -o xtrace
+fi
+
+fedora_image_file="$HOME/Fedora-Cloud.qcow2"
+os_glance_image=fedora-atomic
+os_magnum_template=kubernetes-cluster-template
+os_magnum_cluster=${1:-kubernetes-cluster}
+
+# PEP 370 -- Per user site-packages directory
+[[ "$PATH" != *.local/bin* ]] && export PATH=$PATH:$HOME/.local/bin
+
+if ! command -v magnum; then
+    pip install python-magnumclient
+fi
+if ! command -v kubectl; then
+    curl -fsSL http://bit.ly/install_pkg | PKG=kubectl bash
+fi
+
+if [ ! -f "$fedora_image_file" ]; then
+    curl -o "$fedora_image_file" -sL https://download.fedoraproject.org/pub/fedora/linux/releases/32/Cloud/x86_64/images/Fedora-Cloud-Base-32-1.6.x86_64.qcow2
+fi
+
+if ! openstack image list --name "$os_glance_image" | grep -q "$os_glance_image"; then
+    openstack image create --disk-format=qcow2 --container-format=bare \
+    --file="$fedora_image_file" --property os_distro='fedora-atomic' \
+    "$os_glance_image"
+fi
+
+if ! openstack coe cluster template list | grep -q "$os_magnum_template"; then
+    openstack coe cluster template create "$os_magnum_template" \
+    --image "$os_glance_image" --external-network public1 \
+    --dns-nameserver 8.8.8.8 --master-flavor m1.small \
+    --flavor m1.small --coe kubernetes --docker-volume-size 3
+fi
+
+if ! openstack coe cluster list | grep -q "$os_magnum_cluster"; then
+    openstack coe cluster create "$os_magnum_cluster" \
+    --cluster-template "$os_magnum_template" \
+    --master-count 1 \
+    --node-count 1 \
+    --keypair mykey
+fi
+
+mkdir -p ~/clusters/kubernetes-cluster
+#$(openstack coe cluster config kubernetes-cluster --dir ~/clusters/kubernetes-cluster)
+#export KUBECONFIG=/home/user/clusters/kubernetes-cluster/config
+#kubectl -n kube-system get po
---
+++ scripts/neutron_trunk.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2021
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+set -o nounset
+set -o pipefail
+set -o errexit
+if [[ "${OS_DEBUG:-false}" == "true" ]]; then
+    export PKG_DEBUG=true
+    set -o xtrace
+fi
+
+net_prefix=demo-net
+subnet_prefix=demo-subnet
+port_prefix=demo-port
+
+# PEP 370 -- Per user site-packages directory
+[[ "$PATH" != *.local/bin* ]] && export PATH=$PATH:$HOME/.local/bin
+
+if ! command -v neutron > /dev/null; then
+    pip install python-neutronclient
+fi
+
+# Create networks and subnets for the trunk and subports
+for i in $(seq 1 5); do
+    if ! openstack network list -c Name | grep -q "$net_prefix$i"; then 
+        openstack network create "$net_prefix$i"
+    fi
+    if ! openstack subnet list -c Name | grep -q "$subnet_prefix$i"; then 
+        openstack subnet create --network "$net_prefix$i" --subnet-range "10.0.$i.0/24" "$subnet_prefix$i"
+    fi
+    if ! openstack port list -c name | grep -q "$port_prefix$i"; then
+        openstack port create --network "$net_prefix$i" "$port_prefix$i"
+    fi
+done
+
+if ! openstack port list -c name | grep -q "trunk-parent"; then
+    openstack port create --network demo-net trunk-parent
+fi
+
+# Create the trunk and add subports to the trunk
+if ! openstack network trunk list -c Name | grep -q "trunk1"; then
+    openstack network trunk create --parent-port trunk-parent trunk1
+fi
+for i in $(seq 1 5); do
+    openstack network trunk set \
+    --subport port="$port_prefix$i,segmentation-type=vlan,segmentation-id=$i" \
+    trunk1
+done
+openstack network trunk show trunk1
+
+if ! openstack server list -c Name | grep -q "demo1"; then
+    openstack server create --image cirros --flavor m1.tiny \
+    --key-name mykey --port trunk-parent \
+    demo1
+fi
