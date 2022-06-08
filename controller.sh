3b9f90c2f22655cf41a25bcf4611aac49b503e8c
Victor Morales <chipahuac@hotmail.com>
2022-06-08 16:41:16 -0700

    Clean up /etc/hosts file in controller nodes

---
+++ controller.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+set -o nounset
+set -o pipefail
+set -o errexit
+if [[ "${OS_DEBUG:-false}" == "true" ]]; then
+    export PKG_DEBUG=true
+    set -o xtrace
+fi
+
+_start=$(date +%s)
+trap 'printf "Controller installation process: %s secs\n" "$(($(date +%s)-_start))"' EXIT
+
+# NOTE: Required for HA setups to allow rabbit hostname resolves uniquely to the proper IP address
+echo "" | sudo tee /etc/hosts
