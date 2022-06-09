d072e3a53c98a4c1bbe39ab280aa502b526cb548
Victor Morales <chipahuac@hotmail.com>
2022-06-08 19:08:41 -0700

    Enforce the usage of Cloudflare DNS

--- requirements/base.sh
+++ requirements/base.sh
@@ -12,13 +12,16 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-if ! command -v curl; then
-    # shellcheck disable=SC1091
-    source /etc/os-release || source /usr/lib/os-release
-    case ${ID,,} in
-        ubuntu|debian)
+# shellcheck disable=SC1091
+source /etc/os-release || source /usr/lib/os-release
+case ${ID,,} in
+    ubuntu|debian)
+        if [ "${ID,,}" == "ubuntu" ] && command -v systemd-resolve  && ( ! systemd-resolve --status | grep -q 1.1.1.1 ); then
+            sudo systemd-resolve --interface "$(ip route get 1.1.1.1 | grep "^1." | awk '{ print $5 }')" --set-dns 1.1.1.1
+        fi
+        if ! command -v curl; then
             sudo apt-get update -qq > /dev/null
             sudo apt-get install -y -qq -o=Dpkg::Use-Pty=0 curl
-        ;;
-    esac
-fi
+        fi
+    ;;
+esac
