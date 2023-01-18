d128944e6c4ef90dc844345dbdd8c7d5fa254e79
Victor Morales <chipahuac@hotmail.com>
2023-01-17 19:39:55 -0800

    Add specs for run_kaction and undercloud scripts

--- run_kaction.sh
+++ run_kaction.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # SPDX-license-identifier: Apache-2.0
 ##############################################################################
-# Copyright (c) 2020
+# Copyright (c) 2020,2023
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License, Version 2.0
 # which accompanies this distribution, and is available at
@@ -20,11 +20,21 @@ PYTHONPATH="$(python -c 'import sys; print(":".join(sys.path))')"
 EXTRA_OPTS="--extra ansible_user=$USER --extra ansible_become=true --extra ansible_become_method=sudo"
 export PYTHONPATH EXTRA_OPTS
 
-ansible_cmd="kolla-ansible $1 --inventory ${OS_INVENTORY_FILE:-./samples/aio/hosts.ini}"
-if [[ ${OS_DEBUG:-false} == "true" ]]; then
-    ansible_cmd+=" --verbose"
+function _get_kolla_ansible_cmd {
+    local kolla_action=$1
+
+    cmd="$(command -v kolla-ansible || echo kolla-ansible) $kolla_action --inventory ${OS_INVENTORY_FILE:-./samples/aio/hosts.ini}"
+    if [[ ${OS_DEBUG:-false} == "true" ]]; then
+        cmd+=" --verbose"
+    fi
+    if [[ $1 == "destroy" ]]; then
+        cmd+=" --yes-i-really-really-mean-it"
+    fi
+
+    echo "$cmd"
+}
+
+if [[ ${__name__:-"__main__"} == "__main__" ]]; then
+    ansible_cmd=$(_get_kolla_ansible_cmd "$1")
+    $ansible_cmd
 fi
-if [[ $1 == "destroy" ]]; then
-    ansible_cmd+=" --yes-i-really-really-mean-it"
-fi
-$ansible_cmd
